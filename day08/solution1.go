c0738d42199d5efebe4b65fe16995818af403454
andres-dennehy-oeg <andres.dennehy@octoenergy.com>
2023-12-11 15:04:04 +0100

    Day 8

---
+++ day08/solution1.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strings"
+)
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func parseLeftRight(s string) (string, string) {
+    parts := strings.Split(s, ", ");
+    left, right := parts[0], parts[1];
+    left = strings.Replace(left, "(", "", 1);
+    right = strings.Replace(right, ")", "", 1);
+    return left, right
+}
+
+func main() {
+
+    file, err := os.Open("input.txt");
+    check(err);
+    defer file.Close();
+
+    scanner := bufio.NewScanner(file);
+    //var steps int;
+    nodes := make(map[string][]string, 0)
+
+    scanner.Scan();
+    orders := scanner.Text();
+
+    // Empty line
+    scanner.Scan();
+
+    // First element
+    scanner.Scan();
+    line := strings.Split(scanner.Text(), " = ");
+    left, right := parseLeftRight(line[1]);
+    first_element := line[0]
+    nodes[line[0]] = []string{left, right};
+
+    // Rest
+    for scanner.Scan() {
+        line := strings.Split(scanner.Text(), " = ");
+        left, right := parseLeftRight(line[1]);
+        nodes[line[0]] = []string{left, right};
+    }
+
+    // Find next node according to orders
+    // Brute force...
+    current_node := first_element;
+    current_order := 0;
+    var steps int;
+    for current_node != "ZZZ" {
+        if orders[current_order] == 'L' {
+            current_node = nodes[current_node][0]
+        } else {
+            current_node = nodes[current_node][1]
+        }
+        if current_order < len(orders) - 1 {
+            current_order++
+        } else {
+            current_order = 0
+        }
+        steps++
+    }
+
+    fmt.Printf("Result is %v\n", steps);
+}
---
+++ day08/solution2.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strings"
+)
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func GCD(a, b int) int {
+      for b != 0 {
+              t := b
+              b = a % b
+              a = t
+      }
+      return a
+}
+
+func LCM(a, b int, integers ...int) int {
+      result := a * b / GCD(a, b)
+
+      for i := 0; i < len(integers); i++ {
+              result = LCM(result, integers[i])
+      }
+
+      return result
+}
+
+func parseLeftRight(s string) (string, string) {
+    parts := strings.Split(s, ", ");
+    left, right := parts[0], parts[1];
+    left = strings.Replace(left, "(", "", 1);
+    right = strings.Replace(right, ")", "", 1);
+    return left, right
+}
+
+func main() {
+
+    file, err := os.Open("input.txt");
+    check(err);
+    defer file.Close();
+
+    scanner := bufio.NewScanner(file);
+    //var steps int;
+    nodes := make(map[string][]string, 0)
+    a_nodes := make([]string, 0)
+
+    scanner.Scan();
+    orders := scanner.Text();
+
+    // Empty line
+    scanner.Scan();
+
+    // Rest
+    for scanner.Scan() {
+        line := strings.Split(scanner.Text(), " = ");
+        left, right := parseLeftRight(line[1]);
+        nodes[line[0]] = []string{left, right};
+        if line[0][len(line[0])-1] == 'A' {
+            a_nodes = append(a_nodes, line[0])
+        }
+    }
+    fmt.Println(a_nodes)
+
+    var current_node string;
+    var current_order int;
+    a_node_steps := make([]int, 0);
+    var steps int;
+    for _, a_node := range a_nodes {
+        current_node = a_node;
+        current_order = 0;
+        steps = 0;
+        for current_node[len(current_node)-1] != 'Z' {
+            if orders[current_order] == 'L' {
+                current_node = nodes[current_node][0]
+            } else {
+                current_node = nodes[current_node][1]
+            }
+            if current_order < len(orders) - 1 {
+                current_order++
+            } else {
+                current_order = 0
+            }
+            steps++
+        }
+        fmt.Printf("Node %v took %v steps\n", a_node, steps)
+        a_node_steps = append(a_node_steps, steps)
+    }
+
+
+    fmt.Printf("Result is %v\n", LCM(a_node_steps[0], a_node_steps[1], a_node_steps[2:]...));
+}
